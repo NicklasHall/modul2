e7f53b653a9678afd25ae849294e2b39208de5b3
NicklasHall <nicklas.hall@elev.ga.ntig.se>
2023-08-31 11:46:00 +0200

    modul2

---
+++ modul2.py
@@ -0,0 +1,28 @@
+print(1+2)
+print(5-2)
+print(10/2)
+print(10*2)
+print(10//3)
+print(5**7)
+
+
+a=int(input("säg nummer: "))       #räknar gånger med olika tal
+b=int(input("ett till nummer: "))
+
+print(a*b)
+
+
+a1=float(input("hur lång är du i meter: "))
+b1=float(input("Hur myvket väger du kg: "))
+
+print(b1//(a1**2))  #Det här är en BMI räknare
+
+
+
+a2=int(input("Din ålder: "))
+print("din ålder i veckor:", a2*52)
+
+
+a3=int(input("din vikt i kg: "))
+print(a3*2.20462262)
+
